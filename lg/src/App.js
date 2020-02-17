c36642ec83ca5dfbf6525d08ae0ce7a297fe2c68
Pubudu Arosha <it1810854@my.sliit.lk>
2020-02-17 14:04:52 +0530

    first commit

---
+++ lg/src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Login from "./components/login.component";
+import SignUp from "./components/signup.component";
+
+function App() {
+  return (<Router>
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/sign-in"}><h2>Sierra Cables Testing 5</h2></Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}><h5>Log In</h5></Link>
+              </li>
+              
+              <li className="nav-item">
+                <Link className="nav-link" ><h5>|</h5></Link>
+              </li>
+              
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-up"}><h5>Sign Up</h5></Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/sign-in" component={Login} />
+            <Route path="/sign-up" component={SignUp} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
+  );
+}
+
+export default App;
